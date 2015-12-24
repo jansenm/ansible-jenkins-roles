498c786a2f66c559f29108547364704ecfcca6cf
Michael Jansen <kde@michael-jansen.biz>
2015-12-24 16:21:34 +0100

    Implement "Proxy Configuration"

---
+++ jenkins/configure-proxy/templates/init.groovy.d/configure-proxy.groovy
@@ -0,0 +1,22 @@
+// {{ansible_managed}}
+import hudson.ProxyConfiguration
+import jenkins.model.Jenkins
+{% if proxy_host is defined %}
+    {% if proxy_username is defined %}
+proxy = new ProxyConfiguration(
+    "{{proxy_host}}",
+    {{proxy_port}},
+    "{{proxy_username}}",
+    "{{proxy_password}}",
+    "{{proxy_no_proxy | replace ("\n", "\\n")}}")
+    {% else %}
+proxy = new ProxyConfiguration(
+    "{{proxy_host}}",
+    {{proxy_port}},
+    null,
+    null,
+    "{{proxy_no_proxy | replace ("\n", "\\n")}}")
+    {% endif %}
+{% else %}
+proxy = null
+Jenkins.instance.proxy = proxy
